bacc334133b31c6d2cf6630cf289155323cdaeb6
dideles <didenkoelenas@yandex.ru>
2024-08-14 18:56:14 +0700

    1st com

--- Quest_1/src/com/javarush/tasks/Level_1/Task_2/Solution.java
+++ Quest_1/src/com/javarush/tasks/Level_1/Task_2/Solution.java
@@ -1,7 +1,7 @@
-package com.javarush.tasks.Level_1.Task_1;
+package com.javarush.tasks.Level_1.Task_2;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(2);
     }
 }
