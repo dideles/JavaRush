1aba987c1a40e368ed9916a2c362e2b84c0702be
dideles <didenkoelenas@yandex.ru>
2024-11-15 22:56:10 +0700

    Level 01 task 01

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution01.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution01.java
@@ -1,10 +1,11 @@
 package com.javarush.Level_01.Task_01;
 
-// Прямой вывод строки:
-
+/**
+ * // Прямой вывод строки:
+ */
 public class Solution01 {
     public static void main(String[] args) {
         System.out.println("Амиго очень умный");
-        // System.out.print("Амиго очень умный");
+        // System.out.print("Амиго очень умный"); // Использование метода print без перехода на новую строку
     }
 }
