cefc4fb4c90492a579e34bb3a662fe32113eb9ce
dideles <didenkoelenas@yandex.ru>
2024-11-15 23:50:19 +0700

    Level 01 task 10

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution10.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution10.java
@@ -1,13 +1,22 @@
 package com.javarush.Level_01.Task_01;
 
-// Использование массивов строк через цикл
-
+/**
+ * Вывод массива символов в консоль
+ */
 public class Solution10 {
     public static void main(String[] args) {
-        String[] parts = {"Амиго", " ", "очень", " ", "умный"};
-        for (String part : parts) {
-            System.out.print(part);
+        char[] letters = {
+                'А', 'м', 'и', 'г', 'о',
+                ' ',
+                'о', 'ч', 'е', 'н', 'ь',
+                ' ',
+                'у', 'м', 'н', 'ы', 'й'};
+
+        for (char c : letters) {
+            System.out.print(c);
         }
-        System.out.println();
+
+        // Переход на новую строку
+        System.out.println(); // чтобы отделить его от следующего блока кода
     }
 }
