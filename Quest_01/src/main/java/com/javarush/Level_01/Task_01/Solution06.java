e36b62f8040b0e4d6c77fdc1c16d9b27044e897d
dideles <didenkoelenas@yandex.ru>
2024-11-15 23:48:35 +0700

    Level 01 task 06

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution06.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution06.java
@@ -1,16 +1,29 @@
 package com.javarush.Level_01.Task_01;
 
-// Использование StringBuilder для создания изменяемых строк
-
+/**
+ * Использования метода String.join() для объединения строк с пробелами.
+ */
 public class Solution06 {
     public static void main(String[] args) {
-        /*
-        StringBuilder sb = new StringBuilder();
-        sb.append("Амиго").append(" ").append("очень").append(" ").append("умный");
-        System.out.println(sb); // Вызывает sb.toString() неявно
-        // Warning:(7, 23) 'StringBuilder sb' can be replaced with 'String'
-        */
-
-        System.out.println("Амиго" + " " + "очень" + " " + "умный"); // Вызывает sb.toString() неявно
+        String message = String.join(" ", "Амиго", "очень", "умный");
+        System.out.println(message);
     }
 }
+
+/*
+Оператор +:
+    Для простого добавления одной строки к другой.
+
+Метод String.concat():
+    Этот метод добавляет указанную строку в конец текущей строки.
+    Простота использования для добавления одной строки к другой.
+    Производительность сопоставима с конкатенацией.
+    НО менее гибкий, чем join, поскольку работает только с добавлением строк друг за другом.
+
+Метод String.join():
+    Для объединения нескольких строк с разделителем.
+    Удобен для объединения множества строк с одинаковым разделителем.
+
+Метод StringBuilder.append():
+    Для высокой производительности при частых операциях конкатенации.
+ */
