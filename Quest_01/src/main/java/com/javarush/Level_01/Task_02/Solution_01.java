7680943694e6dbe734b6e239ea8257d7eb97dc9b
dideles <didenkoelenas@yandex.ru>
2024-11-15 23:52:24 +0700

    Level 02 task 01

--- Quest_01/src/main/java/com/javarush/Level_01/Task_02/Solution_01.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_02/Solution_01.java
@@ -1,8 +1,13 @@
 package com.javarush.Level_01.Task_02;
 
-public class Solution {
+/**
+ * Простая печать строки
+ */
+public class Solution_01 {
     public static void main(String[] args) {
         System.out.print("Я думаю, быть программистом круто");
+        // Использование метода print без перехода на новую строку
+
         // System.out.println("Я думаю, быть программистом круто");
     }
 }
