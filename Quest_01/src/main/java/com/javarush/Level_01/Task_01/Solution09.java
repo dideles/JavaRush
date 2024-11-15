bd3254ff2151fd3932322e8030046b03605860af
dideles <didenkoelenas@yandex.ru>
2024-11-15 23:49:53 +0700

    Level 01 task 09

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution09.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution09.java
@@ -1,13 +1,12 @@
 package com.javarush.Level_01.Task_01;
 
-// Массив символов с обработкой
-
+/**
+ * С использованием массива символов
+ */
 public class Solution09 {
     public static void main(String[] args) {
-        char[] message = {'А', 'м', 'и', 'г', 'о', ' ', 'о', 'ч', 'е', 'н', 'ь', ' ', 'у', 'м', 'н', 'ы', 'й'};
-        for (char c : message) {
-            System.out.print(c);
-        }
-        System.out.println();
+        char[] characters = {'А', 'м', 'и', 'г', 'о', ' ', 'о', 'ч', 'е', 'н', 'ь', ' ', 'у', 'м', 'н', 'ы', 'й'};
+        // System.out.println(chars); // передается сам массив символов message
+        System.out.println(new String(characters)); // конструктор String(char[]), который создает строку из массива символов
     }
 }
