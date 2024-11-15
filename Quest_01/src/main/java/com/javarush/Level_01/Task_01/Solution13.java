d524d6a466328a33533a022be4d49b7e27674329
dideles <didenkoelenas@yandex.ru>
2024-11-15 23:51:45 +0700

    Level 01 task 13

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution13.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution13.java
@@ -1,11 +1,17 @@
 package com.javarush.Level_01.Task_01;
 
-// Использование массивов строк
-
+/**
+ * Вывод элементов массива строк с правильным форматированием, избегая лишнего пробела в конце.
+ */
 public class Solution13 {
     public static void main(String[] args) {
         String[] words = {"Амиго", "очень", "умный"};
-        System.out.println(String.join(" ", words));
-        // Первый аргумент метода — это разделитель, который будет использоваться между элементами массива.
+
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                System.out.print(" "); // Добавляем пробел перед всеми словами, кроме первого
+            }
+            System.out.print(words[i]);
+        }
     }
 }
