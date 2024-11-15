f00c820b1ff6b11f9f624bed83ee197e00d18f31
dideles <didenkoelenas@yandex.ru>
2024-11-15 23:50:30 +0700

    Level 01 task 11

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution11.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution11.java
@@ -1,13 +1,21 @@
 package com.javarush.Level_01.Task_01;
 
-// Использование массивов строк через цикл
-
+/**
+ * Использование массивов строк через цикл
+ */
 public class Solution11 {
     public static void main(String[] args) {
-        String[] words = {"Амиго", "очень", "умный"};
-        for (String word : words) {
-            System.out.print(word + " ");
+        String[] parts = {
+                "Амиго",
+                " ",
+                "очень",
+                " ",
+                "умный"};
+        for (String part : parts) {
+            System.out.print(part);
         }
+
+        // Переход на новую строку
+        System.out.println(); // чтобы отделить его от следующего блока кода
     }
 }
-
