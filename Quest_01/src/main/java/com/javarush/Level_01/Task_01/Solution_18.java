0bc69940b7ab28bd78fccbbe90a57cd555b35f2c
dideles <didenkoelenas@yandex.ru>
2024-11-16 18:19:51 +0700

    Level 01 Task 01 Solution 18

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_18.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_18.java
@@ -1,8 +1,9 @@
 package com.javarush.Level_01.Task_01;
 
-// Форматирование с использованием переменных
-
-public class Solution16 {
+/**
+ * Форматирование с использованием переменных.
+ */
+public class Solution_18 {
     public static void main(String[] args) {
         String start = "Амиго ";
         String end = "очень умный";
