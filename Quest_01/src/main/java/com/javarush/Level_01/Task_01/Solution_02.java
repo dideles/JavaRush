214aaec71ea1658785fc2e1d714604792e5a47a3
dideles <didenkoelenas@yandex.ru>
2024-11-16 14:53:36 +0700

    Level 01 task 01 solution 02

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_02.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_02.java
@@ -3,7 +3,7 @@ package com.javarush.Level_01.Task_01;
 /**
  * Использование строковой переменной
  */
-public class Solution02 {
+public class Solution_02 {
     public static void main(String[] args) {
         String message = "Амиго очень умный";
         // String message = "Амиго" + " " + "очень" + " " + "умный";
