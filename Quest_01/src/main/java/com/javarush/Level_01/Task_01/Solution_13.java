3c1e39bcf2ac86508f073c134c2aba4245a49c11
dideles <didenkoelenas@yandex.ru>
2024-11-16 18:18:21 +0700

    Level 01 Task 01 Solution 13

---
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_13.java
@@ -0,0 +1,17 @@
+package com.javarush.Level_01.Task_01;
+
+/**
+ * Вывод элементов массива строк с правильным форматированием, избегая лишнего пробела в конце.
+ */
+public class Solution_13 {
+    public static void main(String[] args) {
+        String[] words = {"Амиго", "очень", "умный"};
+
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                System.out.print(" "); // Добавляем пробел перед всеми словами, кроме первого
+            }
+            System.out.print(words[i]);
+        }
+    }
+}
