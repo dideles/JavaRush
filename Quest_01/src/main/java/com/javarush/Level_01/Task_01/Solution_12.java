34c60d1e59fa15260f582acae8a0a1e6e9ac3e04
dideles <didenkoelenas@yandex.ru>
2024-11-16 18:18:01 +0700

    Level 01 Task 01 Solution 12

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_12.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_12.java
@@ -5,14 +5,15 @@ package com.javarush.Level_01.Task_01;
  * НО нужно учитывать, что после последнего слова также будет добавлен лишний пробел.
  * См. Solution13
  */
-public class Solution12 {
+public class Solution_12 {
     public static void main(String[] args) {
+
         String[] words = {"Амиго", "очень", "умный"};
+
         for (String word : words) {
             System.out.print(word + " ");
-            // System.out.print((word + " ").toString().trim()); // может быть излишне ресурсоемким для длинных строк
+            // System.out.print((word + " ").toString().trim()); // Амигооченьумный
         }
 
     }
 }
-
