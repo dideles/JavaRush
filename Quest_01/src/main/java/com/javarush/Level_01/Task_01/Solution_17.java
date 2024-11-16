e8ec4fcb400d80a125ea5009de319e472e774b1d
dideles <didenkoelenas@yandex.ru>
2024-11-16 19:35:17 +0700

    Level 01 Task 01 Solution 17

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_17.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_17.java
@@ -1,12 +1,11 @@
 package com.javarush.Level_01.Task_01;
 
 /**
- * Использование метод форматирования String.format() для создания строки.
+ * Чтение строки из массива.
  */
 public class Solution_17 {
     public static void main(String[] args) {
-        // String message = String.format("%s%s%s", "Амиго", " очень ", "умный");
-        String message = String.format("%s%s%s%s%s", "Амиго", " ", "очень", " ", "умный");
-        System.out.println(message);
+        String[] message = {"Амиго", "очень", "умный"};
+        System.out.println(message[0] + " " + message[1] + " " + message[2]);
     }
 }
