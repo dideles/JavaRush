3f69839246027b49ae4f5ba0368af49117da6d82
dideles <didenkoelenas@yandex.ru>
2024-11-16 18:17:39 +0700

    Level 01 Task 01 Solution 11

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_11.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_11.java
@@ -1,9 +1,9 @@
 package com.javarush.Level_01.Task_01;
 
 /**
- * Использование массивов строк через цикл
+ * Использование массивов строк через цикл.
  */
-public class Solution11 {
+public class Solution_11 {
     public static void main(String[] args) {
         String[] parts = {
                 "Амиго",
