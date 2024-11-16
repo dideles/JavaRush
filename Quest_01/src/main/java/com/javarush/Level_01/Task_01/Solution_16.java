d8a657ac7e3f7bfec2a13e1fcde3dcbdf5ae6a3b
dideles <didenkoelenas@yandex.ru>
2024-11-16 19:35:02 +0700

    Level 01 Task 01 Solution 16

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_16.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_16.java
@@ -1,11 +1,12 @@
 package com.javarush.Level_01.Task_01;
 
 /**
- * Чтение строки из массива.
+ * Использование массивов строк.
  */
 public class Solution_16 {
     public static void main(String[] args) {
-        String[] message = {"Амиго", "очень", "умный"};
-        System.out.println(message[0] + " " + message[1] + " " + message[2]);
+        String[] words = {"Амиго", "очень", "умный"};
+        System.out.println(String.join(" ", words));
+        // Первый аргумент метода — это разделитель, который будет использоваться между элементами массива.
     }
 }
