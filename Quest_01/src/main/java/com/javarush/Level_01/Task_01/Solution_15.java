22b529a33fa30553fe4ea262934e3d2a0b3557db
dideles <didenkoelenas@yandex.ru>
2024-11-16 19:33:23 +0700

    Level 01 Task 01 Solution 15

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_15.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_15.java
@@ -1,12 +1,20 @@
 package com.javarush.Level_01.Task_01;
 
 /**
- * Использование массивов строк.
+ * Создание строки из массива строк с использованием цикла.
  */
 public class Solution_15 {
     public static void main(String[] args) {
+
         String[] words = {"Амиго", "очень", "умный"};
-        System.out.println(String.join(" ", words));
-        // Первый аргумент метода — это разделитель, который будет использоваться между элементами массива.
+
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            result.append(word).append(" ");
+        }
+
+        System.out.println(result.toString().trim());
     }
 }
+
