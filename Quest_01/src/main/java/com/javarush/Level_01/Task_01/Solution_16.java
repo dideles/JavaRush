42b1e478690c4b508e568c0ce07e9a3ab5521283
dideles <didenkoelenas@yandex.ru>
2024-11-16 18:19:14 +0700

    Level 01 Task 01 Solution 16

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_16.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_16.java
@@ -1,8 +1,9 @@
 package com.javarush.Level_01.Task_01;
 
-// Чтение строки из массива
-
-public class Solution14 {
+/**
+ * Чтение строки из массива.
+ */
+public class Solution_16 {
     public static void main(String[] args) {
         String[] message = {"Амиго", "очень", "умный"};
         System.out.println(message[0] + " " + message[1] + " " + message[2]);
