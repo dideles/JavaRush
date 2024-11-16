a29897876f8852cd09a0ef4f3df739775ff28ba5
dideles <didenkoelenas@yandex.ru>
2024-11-16 18:19:33 +0700

    Level 01 Task 01 Solution 17

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_17.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_17.java
@@ -1,8 +1,9 @@
 package com.javarush.Level_01.Task_01;
 
-// Использование метод форматирования String.format() для создания строки
-
-public class Solution15 {
+/**
+ * Использование метод форматирования String.format() для создания строки.
+ */
+public class Solution_17 {
     public static void main(String[] args) {
         // String message = String.format("%s%s%s", "Амиго", " очень ", "умный");
         String message = String.format("%s%s%s%s%s", "Амиго", " ", "очень", " ", "умный");
