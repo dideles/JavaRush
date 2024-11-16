9ab385e7c6f3fd306d9a2288935c34b6fce5000d
dideles <didenkoelenas@yandex.ru>
2024-11-16 14:58:45 +0700

    Level 01 Task 01 Solution 05

---
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_02/Solution_05.java
@@ -0,0 +1,10 @@
+package com.javarush.Level_01.Task_02;
+
+/**
+ * Форматированный вывод (использование метода printf)
+ */
+public class Solution_05 {
+    public static void main(String[] args) {
+        System.out.printf("%s%n", "Я думаю, быть программистом круто");
+    }
+}
