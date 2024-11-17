703b87d32d3b0dbe6fcc5ce41f1311600ceeedae
dideles <didenkoelenas@yandex.ru>
2024-11-17 22:51:07 +0700

    Level 01 Task 01 Solution 19

---
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_19.java
@@ -0,0 +1,12 @@
+package com.javarush.Level_01.Task_01;
+
+/**
+ * Форматирование с использованием переменных.
+ */
+public class Solution_19 {
+    public static void main(String[] args) {
+        String start = "Амиго ";
+        String end = "очень умный";
+        System.out.printf("%s%s%n", start, end);
+    }
+}
