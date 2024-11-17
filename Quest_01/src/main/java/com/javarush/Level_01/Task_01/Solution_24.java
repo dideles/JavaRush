e131f4d5a0dce7d0b77df4a50a104a4b119929c4
dideles <didenkoelenas@yandex.ru>
2024-11-17 22:53:46 +0700

    Level 01 Task 01 Solution 24

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_24.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_24.java
@@ -1,10 +1,11 @@
 package com.javarush.Level_01.Task_01;
 
-// Использование Scanner для ввода (хотя задача требует только вывода)
-
 import java.util.Scanner;
 
-public class Solution21 {
+/**
+ * Использование Scanner для ввода (хотя задача требует только вывода).
+ */
+public class Solution_24 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Амиго очень умный");
