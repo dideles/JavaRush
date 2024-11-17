0f31508db3334d9f60c14773df7d5e572b7312f0
dideles <didenkoelenas@yandex.ru>
2024-11-17 22:52:07 +0700

    Level 01 Task 01 Solution 21

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_21.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_21.java
@@ -1,11 +1,13 @@
 package com.javarush.Level_01.Task_01;
 
-// Цикл для вывода символов
-
-public class Solution18 {
+/**
+ * Цикл для вывода символов.
+ */
+public class Solution_21 {
     public static void main(String[] args) {
         String message = "Амиго очень умный";
         for (int i = 0; i < message.length(); i++) {
+            // Внутри цикла вызывается метод message.charAt(i), который возвращает символ на позиции i
             System.out.print(message.charAt(i));
         }
         System.out.println();
