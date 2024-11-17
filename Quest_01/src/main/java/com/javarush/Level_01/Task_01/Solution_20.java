1808c22293333434c45ac236864167473b4f0106
dideles <didenkoelenas@yandex.ru>
2024-11-17 22:51:23 +0700

    Level 01 Task 01 Solution 20

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_20.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_20.java
@@ -1,8 +1,9 @@
 package com.javarush.Level_01.Task_01;
 
-// Использование форматирования
-
-public class Solution17 {
+/**
+ * Использование форматирования.
+ */
+public class Solution_20 {
     public static void main(String[] args) {
         // System.out.printf("%s%n", "Амиго очень умный");
         // System.out.printf("%s%s%s%n", "Амиго", " очень ", "умный");
