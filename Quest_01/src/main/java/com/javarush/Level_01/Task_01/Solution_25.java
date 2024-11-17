10c9121fcb6aa2c8aef4eee52eecbc01c14fa62d
dideles <didenkoelenas@yandex.ru>
2024-11-17 22:54:06 +0700

    Level 01 Task 01 Solution 25

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_25.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_25.java
@@ -1,8 +1,9 @@
 package com.javarush.Level_01.Task_01;
 
-// Использование интерфейс Runnable и лямбда-выражение (Java 8+)
-
-public class Solution22 {
+/**
+ * Использование интерфейс Runnable и лямбда-выражение (Java 8+).
+ */
+public class Solution_25 {
     public static void main(String[] args) {
         Runnable message = () -> System.out.println("Амиго очень умный");
         message.run();
