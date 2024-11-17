04a64f9c21f52a75ff70f68e2b0de49c9146dfff
dideles <didenkoelenas@yandex.ru>
2024-11-17 22:53:28 +0700

    Level 01 Task 01 Solution 23

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_23.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_23.java
@@ -1,8 +1,9 @@
 package com.javarush.Level_01.Task_01;
 
-// Использование метода для формирования текста
-
-public class Solution20 {
+/**
+ *  Использование метода для формирования текста.
+ */
+public class Solution_23 {
     public static void main(String[] args) {
         System.out.println(createMessage());
     }
