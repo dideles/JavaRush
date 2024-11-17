87689490e3ea29e0e871f691da90a7a44ac104c5
dideles <didenkoelenas@yandex.ru>
2024-11-17 22:52:53 +0700

    Level 01 Task 01 Solution 22

--- Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_22.java
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_01/Solution_22.java
@@ -1,8 +1,9 @@
 package com.javarush.Level_01.Task_01;
 
-// Использование метода
-
-public class Solution19 {
+/**
+ * Использование метода.
+ */
+public class Solution_22 {
     public static void main(String[] args) {
         printMessage();
     }
