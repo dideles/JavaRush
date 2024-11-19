218425b2c23f5f47d586ae12f1f1ce9e35d3ed82
dideles <didenkoelenas@yandex.ru>
2024-11-19 23:47:37 +0700

    Level 01 Task 02 Solution 04

---
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_02/Solution_04.java
@@ -0,0 +1,16 @@
+package com.javarush.Level_01.Task_02;
+
+/**
+ * Конкатенация
+ */
+public class Solution_04 {
+    public static void main(String[] args) {
+        String a = "Я";
+        String b = "думаю";
+        String c = ",";
+        String d = "быть";
+        String e = "программистом";
+        String f = "круто";
+        System.out.println(a + " " + b + c + " " + d + " " + e + " " + f);
+    }
+}
