650cd95ba748255f08566a81a2dedfddd8e798e5
dideles <didenkoelenas@yandex.ru>
2024-11-19 23:47:55 +0700

    Level 01 Task 02 Solution 06

---
+++ Quest_01/src/main/java/com/javarush/Level_01/Task_02/Solution_06.java
@@ -0,0 +1,13 @@
+package com.javarush.Level_01.Task_02;
+
+/**
+ * Использование метода format класса String
+ */
+public class Solution_06 {
+    public static void main(String[] args) {
+
+        String formattedText = String.format("%s", "Я думаю, быть программистом круто");
+        System.out.println(formattedText);
+
+    }
+}
